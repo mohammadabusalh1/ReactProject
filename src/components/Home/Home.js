44f83afa12c610cdef1178bd65d6995ecd27b30a
suzanayesh <100838193+suzanayesh@users.noreply.github.com>
2023-06-01 21:42:55 +0300

    Suzan commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Home.module.css';
+
+const Home = (props) => {
+  return (
+    <Card className={classes.home}>
+      <h1>𝕎𝕖𝕝𝕔𝕠𝕞𝕖 𝕥𝕠 𝕥𝕙𝕖 𝔾𝕒𝕣𝕕𝕖𝕟 𝕤𝕙𝕠𝕡 !</h1>
+    </Card>
+  );
+};
+
+export default Home;
